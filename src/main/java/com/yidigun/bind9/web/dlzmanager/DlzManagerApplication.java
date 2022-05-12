874b71dd61a9766ca4e4d7bb0cf7c0cfd4b2c6eb
Daekyu Lee <dklee@yidigun.com>
2022-05-12 21:21:16 +0900

    initial commit

---
+++ src/main/java/com/yidigun/bind9/web/dlzmanager/DlzManagerApplication.java
@@ -0,0 +1,12 @@
+package com.yidigun.bind9.web.dlzmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlzManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlzManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yidigun/bind9/web/dlzmanager/DlzManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yidigun.bind9.web.dlzmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlzManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
